68e609b292629c176889aa49088b7eeb11cc1833
Bruno R.Novais <novais.rezende@gmail.com>
2020-12-06 12:17:08 -0300

    Studied about compiler

---
+++ Compilador/compilador.ts
@@ -0,0 +1,18 @@
+let canal: string = 'Gaveta';
+let inscritos: number = 610234;
+
+// canal = inscritos;
+console.log(canal);
+
+// let nome = 'Pedro'; -> dupla declaração da mesma variável no mesmo escopo;
+
+// function soma(a, b) {
+//   return a + b;
+// } // -> Erro do tipo "noImplicityAny"
+
+// function Saudar(isManha: boolean, isTarde: boolean): string {
+//   let saudacao: string;
+//   let a = 1;
+//   isManha ? (saudacao = 'Bom dia') : (saudacao = '');
+//   return saudacao;
+// } // -> Erro do tipo "StrictNullCheck", "noUnusedParameter", "noUnusedLocal"
